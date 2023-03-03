26aa49300e4faa6a2ecd33ec76e01f19ca4e589a
Marcos Sequeira <marcosemmanuelsequeira@outlook.com>
2023-03-03 15:10:39 -0300

    Prueba vercel

--- src/components/layout/body/landing/layout-body-landing.jsx
+++ src/components/layout/body/landing/layout-body-landing.jsx
@@ -7,7 +7,7 @@ const Landing = () => {
     <Container className='d-grid align-items-center justify-content-between w-50 body-container'>
       <div className="text-center m-auto">
         <div className="circle-blue circle-blue-first"></div>
-        <p className="fw-bold title-size">Haz realidad el negocio</p>
+        <p className="fw-bold title-size">Haz realidad el negocio prueba3</p>
         <p className="fw-bold title-size"> de tus sueños</p>
         <p className="text-muted text-center mt-4 w-75 m-auto">Con nosotros los objetivos de tu emprendimiento se cumplirán. Te brindamos seguridad, profesionalismo y toda la ayuda para lograrlo.</p>
         <div className="circle-blue circle-blue-second"></div>
