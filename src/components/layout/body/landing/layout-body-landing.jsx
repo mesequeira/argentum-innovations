4cf1bbd778dd5577c0745b2cf9a69549d541a5f8
Marcos Sequeira <marcosemmanuelsequeira@outlook.com>
2023-03-03 15:13:30 -0300

    Prueba cambio imagen root vercel

--- src/components/layout/body/landing/layout-body-landing.jsx
+++ src/components/layout/body/landing/layout-body-landing.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Image, Container } from 'react-bootstrap'
 import './layout-body-lading-styles.css'
+import peopleDesk from '../../../../assets/img/people-desk.png'
 
 const Landing = () => {
   return (
     <Container className='d-grid align-items-center justify-content-between w-50 body-container'>
       <div className="text-center m-auto">
         <div className="circle-blue circle-blue-first"></div>
-        <p className="fw-bold title-size">Haz realidad el negocio prueba3</p>
+        <p className="fw-bold title-size">Haz realidad el negocio</p>
         <p className="fw-bold title-size"> de tus sueños</p>
         <p className="text-muted text-center mt-4 w-75 m-auto">Con nosotros los objetivos de tu emprendimiento se cumplirán. Te brindamos seguridad, profesionalismo y toda la ayuda para lograrlo.</p>
         <div className="circle-blue circle-blue-second"></div>
@@ -23,7 +24,7 @@ const Landing = () => {
           <div className="circle blues"></div>
           <div className="circle blues"></div>
         </div>
-        <Image className="m-auto d-flex justify-content-center" src="src/assets/img/people-desk.png" alt="People in desk" />
+        <Image className="m-auto d-flex justify-content-center" src={peopleDesk} alt="People in desk" />
 
         <div className="circle-red"></div>
 
